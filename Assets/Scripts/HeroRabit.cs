d5fba759c3c377a651fd015fa9a3ac12a49d9a7d
udalovamariya <MASCHA222011@mail.ru>
2017-05-20 16:08:02 +0300

    HomeWork 2

--- Assets/Scripts/HeroRabit.cs
+++ Assets/Scripts/HeroRabit.cs
@@ -2,25 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeroRabit : MonoBehaviour {
+public class HeroRabit : MonoBehaviour
+{
 
-    public float speed = 1;
     Rigidbody2D myBody = null;
+    SpriteRenderer sr;
+
+    bool isGrounded = false;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
+    public float speed = 3;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         myBody = this.GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
+        LevelController.current.setStartPosition(transform.position); 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //[-1, 1]
+
         float value = Input.GetAxis("Horizontal");
         if (Mathf.Abs(value) > 0)
         {
@@ -28,15 +40,65 @@ public class HeroRabit : MonoBehaviour {
             vel.x = value * speed;
             myBody.velocity = vel;
         }
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
         if (value < 0)
-        {
             sr.flipX = true;
-        }
         else if (value > 0)
-        {
             sr.flipX = false;
+       
+        Animator animator = GetComponent<Animator>();
+        if (Mathf.Abs(value) > 0)
+        {
+            animator.SetBool("run", true);
+        }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+        Vector3 from = transform.position + Vector3.up * 0.3f;
+        Vector3 to = transform.position + Vector3.down * 0.1f;
+        int layer_id = 1 << LayerMask.NameToLayer("Ground");
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        Debug.DrawLine(from, to, Color.red);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            this.JumpActive = true;
+        }
+        if (this.JumpActive)
+        {
+            //Якщо кнопку ще тримають
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = myBody.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+                    myBody.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false;
+                this.JumpTime = 0;
+            }
+        }
+      
+        if (this.isGrounded)
+        {
+            animator.SetBool("jump", false);
+        }
+        else
+        {
+            animator.SetBool("jump", true);
         }
     }
-
 }
